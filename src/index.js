1631e517c0c701be0a305d816eaaee4358abc6e2
Laroikin <said24abdi04@gmail.com>
2022-01-26 13:29:18 +0500

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = [].concat(...bracketsConfig);
+
+  let stack = [],
+    swap;
+
+  for (let smth of str) {
+    swap = closedBracket(smth, bracketsConfig);
+
+    if (swap != 0 && stack[stack.length - 1] != smth) {
+      stack.push(swap);
+    } else if (stack.pop() != smth) {
+      return false;
+    }
+  }
+  return stack.length == 0 ? true : false;
+};
+
+function closedBracket(char, a) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] == char && i + 1 < a.length) return a[i + 1];
+  }
+  return 0;
 }
+
+// console.log(check('()', [['(', ')']]));
